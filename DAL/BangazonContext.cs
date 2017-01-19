dd115083ecf382ca4641e1287afc31f727f19932
bernardanderson <bernard.anderson@gmail.com>
2017-01-18 20:45:05 -0600

    Initial Commit with DbContext/Models

---
+++ DAL/BangazonContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BangazonTaskTracker.Models;
+
+namespace BangazonTaskTracker.DAL
+{
+    public class BangazonContext : DbContext
+    {
+        public DbSet<UserTask> Users { get; set; }
+    }
+}
---
+++ Models/UserTask.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BangazonTaskTracker.Models
+{
+    public enum TaskStatus
+    {
+        ToDo,
+        InProgress,
+        Complete
+    };
+
+    public class UserTask
+    {
+        [Key]
+        public int Id;
+        [Required]
+        public string Name;
+        [Required]
+        public string Description;
+        [Required]
+        public TaskStatus Status;
+        public DateTime CompletedOn;
+    }
+}
